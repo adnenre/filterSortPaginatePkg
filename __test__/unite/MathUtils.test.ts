fed1d29bafdc87bebe708999ec1b72d520294ecb
Adnen Rebai <adnen.rebai@gmail.com>
2024-02-19 17:03:47 +0100

    refactor : rename random.test.ts to MathUtils.test.ts

---
+++ __test__/unite/MathUtils.test.ts
@@ -0,0 +1,15 @@
+import { randomInt } from "../../src/MathUtils";
+let max = 1000;
+let min = 1;
+let a = randomInt({ min, max });
+let b = randomInt({ min, max });
+describe("🚀 Get a random number 🚀", () => {
+  test("should return a random number less or equal to max", () => {
+    expect(a).toBeLessThanOrEqual(max);
+  });
+  test("should return a random number between min and max value", () => {
+    let sum = a + b;
+    expect(a + b).toBe(sum);
+    expect(a + b).toBeLessThanOrEqual(sum);
+  });
+});
