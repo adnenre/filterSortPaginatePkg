3b8cb771aeca69019b6e5d8309224366f326674e
Adnen Rebai <adnen.rebai@gmail.com>
2024-02-21 10:34:27 +0100

    doc: add documentation

--- src/ArrayUtils.ts
+++ src/ArrayUtils.ts
@@ -1,3 +1,5 @@
+/* The ArrayUtils class in TypeScript provides a static method to map elements of an array to a new
+array based on a given mapping function. */
 export class ArrayUtils {
   static map<T, U>(arr: T[], mapper: (item: T) => U): U[] {
     const result: U[] = [];
@@ -7,6 +9,17 @@ export class ArrayUtils {
     return result;
   }
 
+  /**
+   * The `filter` function takes an array and a predicate function, and returns a new array containing
+   * only the elements that satisfy the predicate.
+   * @param {T[]} arr - The `arr` parameter is an array of elements of type `T`.
+   * @param predicate - The `predicate` parameter in the `filter` function is a callback function that
+   * takes an item of type `T` as its argument and returns a boolean value. This function is used to
+   * determine whether an item should be included in the resulting array based on the specified
+   * condition.
+   * @returns The `filter` method is returning an array of elements that satisfy the given `predicate`
+   * function.
+   */
   static filter<T>(arr: T[], predicate: (item: T) => boolean): T[] {
     const result: T[] = [];
     for (const item of arr) {
@@ -17,6 +30,17 @@ export class ArrayUtils {
     return result;
   }
 
+  /**
+   * The `groupBy` function in TypeScript takes an array of items and groups them based on a specified
+   * key function.
+   * @param {T[]} arr - The `arr` parameter is an array of elements of type `T` that you want to group
+   * based on a specific key.
+   * @param keyFn - The `keyFn` parameter is a function that takes an item of type `T` as input and
+   * returns a string. This function is used to determine the key for grouping the items in the array
+   * `arr`.
+   * @returns The `groupBy` function returns an object where the keys are strings and the values are
+   * arrays of type `T`.
+   */
   static groupBy<T>(
     arr: T[],
     keyFn: (item: T) => string
--- src/MathUtils.ts
+++ src/MathUtils.ts
@@ -1,7 +1,20 @@
+/* The `interface RandomNumber` is defining a structure for an object that has two properties: `min`
+and `max`, both of which are of type `number`. This interface is used to specify the shape of the
+object that the `randomInt` function expects as its argument. By defining this interface, you are
+ensuring that any object passed to the `randomInt` function must have `min` and `max` properties of
+type `number`. */
 interface RandomNumber {
   min: number;
   max: number;
 }
+/**
+ * The function `randomInt` generates a random integer within a specified range.
+ * @param {RandomNumber}  - The `randomInt` function takes an object with two properties `min` and
+ * `max` as input. These properties are defined in the `RandomNumber` interface. The function generates
+ * a random integer between the `min` and `max` values (inclusive) and returns it. If `min
+ * @returns The function `randomInt` returns a random integer between the `min` and `max` values
+ * provided in the `RandomNumber` object.
+ */
 export const randomInt = ({ min, max }: RandomNumber): number => {
   if (min === max) return max;
   return Math.floor(Math.random() * (max - min + 1) + min);
