3abf54578cfd202e34e9afc8dd4349b62c43c82e
Adnen Rebai <adnen.rebai@gmail.com>
2024-02-26 17:18:12 +0100

    feature : add power / calcHypotenus / sqrt / abs function to MathUtils

--- src/__test__/unite/MathUtils.test.ts
+++ src/__test__/unite/MathUtils.test.ts
@@ -1,8 +1,8 @@
 import U from "@/modules";
 let max = 1000;
 let min = 1;
-let a = U.Math.randomInt({ min, max });
-let b = U.Math.randomInt({ min, max });
+let a = U.Math.random({ min, max });
+let b = U.Math.random({ min, max });
 describe("🚀 Get a random number 🚀", () => {
   test("should return a random number less or equal to max", () => {
     expect(a).toBeLessThanOrEqual(max);
@@ -11,5 +11,22 @@ describe("🚀 Get a random number 🚀", () => {
     let sum = a + b;
     expect(a + b).toBe(sum);
     expect(a + b).toBeLessThanOrEqual(sum);
+  });
+
+  test("should return returns the value of a base raised to a power ", () => {
+    let expected = U.Math.power(4, 0.5);
+    let expected2 = U.Math.power(-7, 0.5);
+
+    expect(expected).toBe(2);
+    expect(expected2).toBe(NaN);
+  });
+  test("Should returns the square root of a number ", () => {
+    let a = 7;
+    let b = 24;
+    let powerSum = a * a + b * b;
+    let hypotenuse = U.Math.sqrt(powerSum); // 25
+
+    let expected = U.Math.calcHypotenuse(a, b);
+    expect(expected).toBe(hypotenuse);
   });
 });
--- src/modules/utils/Math.ts
+++ src/modules/utils/Math.ts
@@ -2,7 +2,7 @@
  * Interface representing a range of numbers.
  * @interface
  */
-export interface RandomNumberRange {
+export interface RandomRange {
   /**
    * The minimum value of the range.
    * @type {number}
@@ -18,16 +18,70 @@ export interface RandomNumberRange {
 class MathUtils {
   /**
    * Generates a random integer within the specified range.
-   * @param {RandomNumberRange} args - The range of numbers from which to generate a random integer.
+   * @param {RandomRange} args - The range of numbers from which to generate a random integer.
    * @returns {number} A random integer within the specified range.
    * @example
-   * const result = MathUtils.randomInt({ min: 0, max: 10 });
+   * const result = MathUtils.random({ min: 0, max: 10 });
    * console.log(result); // Output: Random integer between 0 and 9
    */
-  static randomInt = (args: RandomNumberRange): number => {
+  static random = (args: RandomRange): number => {
     if (args.min === args.max) return args.max;
     return Math.floor(Math.random() * (args.max - args.min + 1) + args.min);
   };
+
+  /**
+   * The static abs function in TypeScript returns the absolute value of a given number.
+   * @param {number} x - The parameter `x` is a number that represents the value for which you want to
+   * calculate the absolute value.
+   * @returns The absolute value of the input number `x` is being returned.
+   */
+  static abs(x: number): number {
+    return Math.abs(x);
+  }
+
+  /**
+   * The static sqrt function in TypeScript calculates the square root of a given number.
+   * @param {number} x - The parameter `x` represents the number for which you want to calculate the
+   * square root.
+   * @returns The square root of the input number `x` is being returned.
+   */
+  static sqrt(x: number): number {
+    return Math.sqrt(x);
+  }
+  /**
+   * The function `pow` calculates the power of a base number raised to an exponent using the
+   * `Math.pow` method.
+   * @param {number} base - The `base` parameter represents the base number in a power operation. It is
+ 
+  /**
+   * The static power function in TypeScript calculates the result of raising a base number to a
+   * specified exponent.
+   * @param {number} base - The `base` parameter is the number that will be raised to the power of the
+   * `exponent`.
+   * @param {number} exponent - The exponent parameter in the power function represents the power to
+   * which the base number is raised. It determines how many times the base number is multiplied by
+   * itself.
+   * @returns The `power` function is returning the result of raising the `base` to the power of
+   * `exponent` using `Math.pow` method.
+   */
+  static power(base: number, exponent: number): number {
+    return Math.pow(base, exponent);
+  }
+
+  /**
+   * This TypeScript function calculates the hypotenuse of a right triangle given the lengths of its two
+   * shorter sides.
+   * @param {number} a - The parameter `a` represents the length of one side of a right triangle.
+   * @param {number} b - The parameter `b` represents the length of one of the sides of a right
+   * triangle.
+   * @returns The function `calcHypotenuse` is returning the square root of the sum of the squares of
+   * the two input numbers `a` and `b`.
+   */
+  static calcHypotenuse(a: number, b: number) {
+    return MathUtils.sqrt(a * a + b * b);
+  }
+
+  // Add more methods as needed...
 }
 
 export { MathUtils };
