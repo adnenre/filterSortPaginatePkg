89de399f6432d05dcd9bc382dfee2b8822aa0c74
Adnen Rebai <adnen.rebai@gmail.com>
2024-02-26 14:16:47 +0100

    refactor : update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
-// jest.config.js
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-  };
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
